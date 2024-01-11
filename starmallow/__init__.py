9267a18fafa642307c5623133342a2346f317811
Michiel <918128+mvanderlee@users.noreply.github.com>
2024-01-11 20:18:09 +0100

    Expose Security param on project

--- starmallow/__init__.py
+++ starmallow/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.2.2"
 
 from .applications import StarMallow
 from .exceptions import RequestValidationError
-from .params import Body, Cookie, Form, Header, NoParam, Path, Query, ResolvedParam
+from .params import Body, Cookie, Form, Header, NoParam, Path, Query, ResolvedParam, Security
 from .responses import HTTPValidationError
 from .routing import APIRoute, APIRouter
 from .websockets import APIWebSocket
