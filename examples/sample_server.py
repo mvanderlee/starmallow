9dbae129b443516ae4245c0f35da89a2d47ab4ba
Michiel <918128+mvanderlee@users.noreply.github.com>
2024-01-16 13:48:40 +0100

    Add Security and Annotate to example server

--- examples/sample_server.py
+++ examples/sample_server.py
@@ -1,21 +1,39 @@
-# Add starmallow package without installing it
-import os.path
-import sys
-from typing import Optional
-
-sys.path.insert(1, os.path.abspath('../'))
-
-
 from dataclasses import asdict
+from typing import Annotated, Optional
 
+import aiofiles.tempfile
 import marshmallow.fields as mf
+import orjson
 from marshmallow.validate import Range
 from marshmallow_dataclass import dataclass as ma_dataclass
+from starlette.middleware import Middleware
+from starlette.middleware.gzip import GZipMiddleware
+from starlette_context import middleware, plugins
 
 from starmallow.applications import StarMallow
-from starmallow.params import Body, Cookie, Header, Path, Query
+from starmallow.params import Body, Cookie, Header, Path, Query, Security
+from starmallow.security.http import HTTPAuthorizationCredentials, HTTPBearer
+from starmallow.security.oauth2 import OAuth2PasswordBearer
 
-app = StarMallow()
+app = StarMallow(
+    title="My API",
+    version="1.0.0",
+    middleware=[
+        # Order matters!
+        Middleware(GZipMiddleware, minimum_size=500),
+        Middleware(
+            middleware.ContextMiddleware,
+            plugins=(
+                plugins.RequestIdPlugin(),
+                plugins.CorrelationIdPlugin(),
+            ),
+        ),
+    ],
+)
+
+
+jwt_scheme = HTTPBearer()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v1/auth/login")
 
 
 @ma_dataclass
@@ -29,10 +47,24 @@ class CreateResponse:
     my_string: str
 
 
+@app.get('/oauth_test')
+def oauth_test(
+    token: str = Security(oauth2_scheme),
+):
+    print(token)
+
+
+@app.get('/auth_test')
+def auth_test(
+    token: Annotated[HTTPAuthorizationCredentials, Security(jwt_scheme)],
+):
+    print(token)
+
+
 @app.post('/test')
 async def test(
     create_request: CreateRequest,
-    limit: int,
+    limit: Annotated[int, Query()],
     offset: int = 0,
     offset2: int = Query(0, model=mf.Integer(validate=[Range(min=0, max=50)])),
     my_string: str = Body('foobar'),
@@ -96,5 +128,14 @@ def test5(
     create_request: CreateRequest = Query(CreateRequest(my_string='default_string', my_int=101)),
 ) -> CreateResponse:
     print(create_request)
+
+    return asdict(create_request)
+
+
+# Test basic JSON request body and JSON response body
+@app.put('/tmp_file')
+async def put_tmp_file(create_request: CreateRequest) -> CreateResponse:
+    async with aiofiles.tempfile.TemporaryFile('wb') as f:
+        await f.write(orjson.dumps(create_request))
 
     return asdict(create_request)
